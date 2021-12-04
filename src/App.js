9803624f309cf850c3c18e22d8f57bd82c503d91
“eyluldnzgit config --global user.email “eyluldnzcn@gmail.com <eyluldnzcn@gmail.com>
2021-12-05 01:30:29 +0300

    Medium için

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const info={
+  name:'Eylül Deniz Can',
+  location:"Turkey",
+  printInfo:()=>`${info.name} from ${info.location}`
+};
+
+const myElement=(
+  <h1>
+    Hello, {info.printInfo()} :)
+    <h2>Have a nice day !</h2>
+  </h1>
+
+);
   return (
     <div className="App">
-      
+      {myElement}
     </div>
   );
 }
